b7beab9bebfb4a4af38726e864b292f89d75c350
K Holsman <kholsman@users.noreply.github.com>
2023-01-05 15:55:17 -0800

    update

---
+++ R/username_password.R
@@ -0,0 +1,5 @@
+# SQL database username and password:
+# update these and copy to the not_shared folder
+username  <- NULL # "addusername"
+password  <-  NULL
+RACE_schema <- "addRACEBASEschemahere(i.e. racebase user folder like HOLSMANK"
